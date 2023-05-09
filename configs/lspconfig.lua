874456148784e86be056214f1155f124b292ee01
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2023-05-09 14:17:36 +0530

    Changed clang capabilities

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -2,19 +2,18 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "rust_analyzer", "emmet_ls", "tsserver" }
+local servers = { "html", "cssls", "pyright", "rust_analyzer", "emmet_ls", "tsserver", "asm_lsp" }
+
+-- Create capabilities for clangd to use utf-16 encoding
+local clang_capabilities = vim.tbl_extend("keep", capabilities, {
+  offsetEncoding = "utf-16",
+})
 
 lspconfig["clangd"].setup {
   on_attach = on_attach,
-  capabilities = {
-    textDocument = {
-      completion = {
-        editsNearCursor = true,
-      },
-    },
-    offsetEncoding = "utf-16",
-  },
+  capabilities = clang_capabilities,
 }
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
